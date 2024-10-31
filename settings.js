e756a3b0abbd98d3859ac56a7b670605c4c37058
Vikingske11 <vikingske11@gmail.com>
2024-11-01 01:37:29 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -49,7 +49,7 @@ global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
-global.autoswview = false //auto view status/story
+global.autoswview = true //auto view status/story
 
 //text bug
 global.xbugtex = {
